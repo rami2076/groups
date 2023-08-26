f7758f0c97bb2818f90ee3a3cf6742b052f578b3
rami2076 <sakusennx@gmail.com>
2023-08-27 06:56:04 +0900

    メンバーを入力するページへのCSSが効いていなかったので修正し、Pタグを除去

--- src/components/molecules/ParticipantsInput.tsx
+++ src/components/molecules/ParticipantsInput.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ParticipantsInput.css';
 
 interface ParticipantsInputProps {
     value: string;
@@ -8,7 +9,7 @@ interface ParticipantsInputProps {
 const ParticipantsInput: React.FC<ParticipantsInputProps> = ({value, onChange}) => {
     return (
         <div className="ParticipantsInput">
-            <p><label htmlFor="participantsInput">1行ずつ分割するメンバーを入力してください</label></p>
+            <label htmlFor="participantsInput">1行ずつ分割するメンバーを入力してください</label>
             <textarea id="participantsInput" rows={6} value={value} onChange={onChange}/>
         </div>
     );
